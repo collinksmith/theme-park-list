bf66a8964254e67ea8c101712d240ad330431c3c
Collin Smith <collink33@gmail.com>
2015-08-10 23:49:09 -0700

    Update styling for forms

--- app/assets/javascripts/views/auth/log_in.js
+++ app/assets/javascripts/views/auth/log_in.js
@@ -1,7 +1,7 @@
 ThemeParkList.Views.LogIn = Backbone.View.extend({
   template: JST["auth/auth_form"],
   tagName: "form",
-  className: "form-group",
+  // className: "form-group",
 
   render: function () {
     this.$el.html(this.template({ new_form: false }));
