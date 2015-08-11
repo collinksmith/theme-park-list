3584317924629bcce56d79444f7a1c929d56f6ff
Collin Smith <collink33@gmail.com>
2015-08-11 16:54:15 -0700

    Add styles for park name

---
+++ app/assets/javascripts/views/parks/parks_index_item_hover.js
@@ -0,0 +1,8 @@
+ThemeParkList.Views.ParksIndexItemHover = Backbone.View.extend({
+  template: JST["parks_index_item_hover"],
+  className: "parks-index-item",
+
+  render: function () {
+    this.$el.html()
+  }
+});
