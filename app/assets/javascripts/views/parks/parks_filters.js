927e96942b6dff28bf049c694655cf3dff6c0444
Collin Smith <collink33@gmail.com>
2015-08-12 12:03:50 -0700

    Add slider

--- app/assets/javascripts/views/parks/parks_filters.js
+++ app/assets/javascripts/views/parks/parks_filters.js
@@ -7,20 +7,26 @@ ThemeParkList.Views.ParksFilters = Backbone.View.extend({
   },
 
   initialize: function () {
-
-    this.$("#slider-range").slider({
-      range: true
-      min: 20,
-      max: 300,
-    })
   },
 
   render: function () {
     this.$el.html(this.template());
+    this.addSlider();
     return this;
   },
 
-  handleSlide: function () {
-    
+  addSlider: function () {
+    this.$("#slider-range").slider({
+      range: true,
+      min: 20,
+      max: 300,
+      values: [ 20, 300 ]
+    })
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  },
+
+  handleSlide: function (event, ui) {
+    $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
   }
 });
