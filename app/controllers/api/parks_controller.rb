2c1790e1f49ea8c15eb6edc430070dad8a4a6351
Collin Smith <collink33@gmail.com>
2015-08-13 14:02:47 -0700

    Add season variable to controller actions

--- app/controllers/api/parks_controller.rb
+++ app/controllers/api/parks_controller.rb
@@ -1,9 +1,11 @@
 class Api::ParksController < ApplicationController
   def index
     @parks = Park.all
+    @season = params[:season] || "year"
   end
 
   def show
     @park = Park.find(params[:id])
+    @season = params[:season] || "year"
   end
 end
