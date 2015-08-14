ab62ba241edb09596b3eb1f51ff748ea60f26600
Collin Smith <collink33@gmail.com>
2015-08-14 15:41:00 -0700

    Update scores view styling

--- app/assets/javascripts/views/parks/show_tabs/scores.js
+++ app/assets/javascripts/views/parks/show_tabs/scores.js
@@ -1,8 +1,10 @@
 ThemeParkList.Views.Scores = Backbone.View.extend({
   template: JST["parks/show_tabs/scores"],
+  className: "container",
 
   render: function () {
     this.$el.html(this.template({ park: this.model }));
     return this;
   }
 });
+
