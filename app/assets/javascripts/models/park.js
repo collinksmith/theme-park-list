d7d8c357a53ae679047a23ec3aff266fe144e6fb
Collin Smith <collink33@gmail.com>
2015-08-14 16:02:10 -0700

    Add parse function to park

--- app/assets/javascripts/models/park.js
+++ app/assets/javascripts/models/park.js
@@ -1,3 +1,30 @@
 ThemeParkList.Models.Park = Backbone.Model.extend({
   urlRoot: "/api/parks",
+
+  parse: function (response) {
+    if (response.costs) {
+      this.costs().set(response.costs);
+      delete response.costs;
+    }
+    if (response.weather) {
+      this.weather().set(response.weather);
+      delete response.weather;
+    }
+    return response;
+  },
+
+  costs: function () {
+    if (!this._costs) {
+      this._costs = new ThemeParkList.Collections.Costs([], { park: this });
+    }
+    return this._costs;
+  },
+
+  weather: function () {
+    if (!this._weather) {
+      this._weather = new ThemeParkList.Collections.
+                               WeatherData([], { park: this });
+    }
+    return this._weather;
+  }
 });
