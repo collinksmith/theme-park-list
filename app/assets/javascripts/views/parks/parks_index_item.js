eba404e63caf4081d3cad644e9b9c9643b478203
Collin Smith <collink33@gmail.com>
2015-08-15 17:44:06 -0700

    Update parks index item styling * Make width more responsive

--- app/assets/javascripts/views/parks/parks_index_item.js
+++ app/assets/javascripts/views/parks/parks_index_item.js
@@ -1,6 +1,6 @@
 ThemeParkList.Views.ParksIndexItem = Backbone.View.extend({
   template: JST['parks/parks_index_item'],
-  className: "parks-index-item col-xs-4",
+  className: "parks-index-item",
 
   events: {
     "click": "addParkShowView"
