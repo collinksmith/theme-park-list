6f2c8389f7859135ce35e40ed001cfcb83005ba0
Collin Smith <collink33@gmail.com>
2015-08-15 21:12:31 -0700

    Add animation to score bars

--- app/assets/javascripts/views/parks/parks_index_item.js
+++ app/assets/javascripts/views/parks/parks_index_item.js
@@ -3,16 +3,26 @@ ThemeParkList.Views.ParksIndexItem = Backbone.View.extend({
   className: "parks-index-item",
 
   events: {
-    "click": "addParkShowView"
+    "click": "addParkShowView",
+    "mouseenter": "toggleScoreBars",
+    "mouseleave": "toggleScoreBars"
   },
 
   render: function () {
     this.$el.html(this.template({ park: this.model }));
+    this.toggleScoreBars();
     return this;
   },
 
   addParkShowView: function () {
     var parkShowView = new ThemeParkList.Views.ParkShow({ model: this.model });
     $("body").append(parkShowView.render().$el);
+  },
+
+  toggleScoreBars: function () {
+    console.log("toggling bars");
+    this.$(".bar").animate({
+      width: "toggle"
+    }, 500);
   }
 });
