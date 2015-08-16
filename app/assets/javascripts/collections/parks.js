c23fd7c404815a2a789456ce8a84ba97ffb2f154
Collin Smith <collink33@gmail.com>
2015-08-16 11:31:45 -0700

    Add parse method to parks backbone collection to deal with updated json response

--- app/assets/javascripts/collections/parks.js
+++ app/assets/javascripts/collections/parks.js
@@ -2,7 +2,9 @@ ThemeParkList.Collections.Parks = Backbone.Collection.extend({
   url: "/api/parks",
   model: ThemeParkList.Models.Park,
 
-  parse: function () {
-    
+  parse: function (response) {
+    this.page = response.page;
+    this.total_pages = response.total_pages;
+    return response.parks
   }
 });
