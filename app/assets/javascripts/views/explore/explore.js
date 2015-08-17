d2e36b6ea16d75e63a3f47378ba0662309c01351
Collin Smith <collink33@gmail.com>
2015-08-17 10:02:29 -0700

    Move filterable mixin from parks index view to explore view

--- app/assets/javascripts/views/explore/explore.js
+++ app/assets/javascripts/views/explore/explore.js
@@ -1,18 +1,51 @@
-ThemeParkList.Views.Explore = Backbone.CompositeView.extend({
-  template: JST["explore/explore"],
+ThemeParkList.Views.Explore = Backbone.CompositeView.extend(
+  _.extend({}, ThemeParkList.Mixins.Filterable {
+    template: JST["explore/explore"],
 
-  initialize: function () {
-    var parksIndexView = new ThemeParkList.Views.
-                             ParksIndex({ collection: this.collection });
-    this.addSubview(".parks-index", parksIndexView);
+    events: {
+      "click #btn-filter": "applyFilter"
+    },
 
-    var filterView = new ThemeParkList.Views.ParksFilters();
-    this.addSubview(".filters", filterView);
-  },
+    initialize: function (options) {
+      this.Basecollection = options.collection;
+      this.filteredCollection = this.baseCollection.filtered(
+        function () { return true ;});
 
-  render: function () {
-    this.$el.html(this.template());
-    this.attachSubviews();
-    return this;
-  }
-});
+      this.parksIndexView = new ThemeParkList.Views.ParksIndex({ 
+        collection: this.filteredCollection 
+      });
+      this.addSubview(".parks-index", this.parksIndexView);
+
+      var filterView = new ThemeParkList.Views.ParksFilters();
+      this.addSubview(".filters", filterView);
+    },
+
+    applyFilter: function (event) {
+      console.log("found click");
+      // var filterFunction = this.computeFilter(event); // filtering logic goes here
+      // this.filteredFunction.teardown();
+      // this.filteredCollection = this.baseCollection.filtered(filterFunction);
+      // this.removeSubview(".parks-index", this.parksIndexView);
+      // this.parksIndexView = new ThemeParkList.Views.ParksIndex({ 
+      //   collection: this.filteredCollection 
+      // });
+      // this.addSubview(".parks-index", this.parksIndexView);
+      // this.render();
+    },
+
+    computeFilter: function () {
+
+    },
+
+    render: function () {
+      this.$el.html(this.template());
+      this.attachSubviews();
+      return this;
+    },
+
+    swapSubviews: function (newSubview, oldSubview) {
+      this.removeSubview(".parks-index", oldSubview);
+      this.addSubview(".parks-index", newSubview);
+    }
+  })
+);
--- app/assets/javascripts/views/parks/parks_index.js
+++ app/assets/javascripts/views/parks/parks_index.js
@@ -1,46 +1,32 @@
-ThemeParkList.Views.ParksIndex = Backbone.CompositeView.extend(
-  _.extend({}, ThemeParkList.Mixins.Filterable, {
-    template: JST["parks/parks_index"],
+ThemeParkList.Views.ParksIndex = Backbone.CompositeView.extend({
+  template: JST["parks/parks_index"],
 
-    initialize: function () {
-      this.baseCollection = options.collection;
-      this.filteredCollection = this.baseCollection.filtered(
-        function () { return true ;});
+  initialize: function () {
+    this.collection.each(this.addParkView.bind(this));
+    this.listenTo(this.collection, "add", this.addParkView);
+    this.listenTo(this.collection, "sync", this.render);
+    $(window).scroll(this.fetchIfAtBottom.bind(this));
+  },
 
-      this.baseCollection.each(this.addParkView.bind(this));
+  render: function () {
+    this.$el.html(this.template({ parks: this.collection }));
+    this.attachSubviews();
+    return this;
+  },
 
-      this.listenTo(this.collection, "add", this.addParkView);
-      this.listenTo(this.collection, "sync", this.render);
-      $(window).scroll(this.fetchIfAtBottom.bind(this));
-    },
+  addParkView: function (park) {
+    var parkView = new ThemeParkList.Views.ParksIndexItem({ model: park });
+    this.addSubview(".parks", parkView);
+  },
 
-    render: function () {
-      this.$el.html(this.template({ parks: this.collection }));
-      this.attachSubviews();
-      return this;
-    },
-
-    applyFilter: function() {
-      var filterFunction = this.computeFilterFunction(); // filtering logic goes here
-      this.filteredFunction.teardown();
-      this.filteredCollection = this.baseCollection.filtered(filterFunction);
-      this.render();
-    },
-
-    addParkView: function (park) {
-      var parkView = new ThemeParkList.Views.ParksIndexItem({ model: park });
-      this.addSubview(".parks", parkView);
-    },
-
-    fetchIfAtBottom: function () {
-      var page = this.collection.page;
-      if ((window.innerHeight + window.scrollY) >= $(document).height() && 
-           page > 0 && page < this.collection.total_pages) {
-        this.collection.fetch({
-          remove: false,
-          data: { page: page + 1 }
-        });
-      }
+  fetchIfAtBottom: function () {
+    var page = this.collection.page
+    if ((window.innerHeight + window.scrollY) >= $(document).height() && 
+         page > 0 && page < this.collection.total_pages) {
+      this.collection.fetch({
+        remove: false,
+        data: { page: page + 1 }
+      })
     }
-  })
-);
+  }
+});
