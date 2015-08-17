df6fdea38d88de9e02f5b4db4c390357e71b0e40
Collin Smith <collink33@gmail.com>
2015-08-17 10:30:07 -0700

    Move filterable mixin to parks collection. Initialize explore page with filtered collection

--- app/assets/javascripts/views/explore/explore.js
+++ app/assets/javascripts/views/explore/explore.js
@@ -1,51 +1,52 @@
-ThemeParkList.Views.Explore = Backbone.CompositeView.extend(
-  _.extend({}, ThemeParkList.Mixins.Filterable {
-    template: JST["explore/explore"],
+ThemeParkList.Views.Explore = Backbone.CompositeView.extend({
+  template: JST["explore/explore"],
 
-    events: {
-      "click #btn-filter": "applyFilter"
-    },
+  events: {
+    "click #btn-filter": "applyFilter"
+  },
 
-    initialize: function (options) {
-      this.Basecollection = options.collection;
-      this.filteredCollection = this.baseCollection.filtered(
-        function () { return true ;});
+  initialize: function (options) {
+    this.baseCollection = options.collection;
 
-      this.parksIndexView = new ThemeParkList.Views.ParksIndex({ 
-        collection: this.filteredCollection 
-      });
-      this.addSubview(".parks-index", this.parksIndexView);
+    var filterView = new ThemeParkList.Views.ParksFilters();
+    this.addSubview(".filters", filterView);
+    this.listenTo(this.baseCollection, "sync", this.setFilteredCollection);
+  },
 
-      var filterView = new ThemeParkList.Views.ParksFilters();
-      this.addSubview(".filters", filterView);
-    },
+  setFilteredCollection: function () {
+    this.filteredCollection = this.baseCollection.filtered(
+      function () { return true ;});
 
-    applyFilter: function (event) {
-      console.log("found click");
-      // var filterFunction = this.computeFilter(event); // filtering logic goes here
-      // this.filteredFunction.teardown();
-      // this.filteredCollection = this.baseCollection.filtered(filterFunction);
-      // this.removeSubview(".parks-index", this.parksIndexView);
-      // this.parksIndexView = new ThemeParkList.Views.ParksIndex({ 
-      //   collection: this.filteredCollection 
-      // });
-      // this.addSubview(".parks-index", this.parksIndexView);
-      // this.render();
-    },
+    this.parksIndexView = new ThemeParkList.Views.ParksIndex({ 
+      collection: this.filteredCollection 
+    });
+    this.swapParksIndexView(this.parksIndexView);
+  },
 
-    computeFilter: function () {
+  applyFilter: function (event) {
+    var filterFunction = this.computeFilter(event); // filtering logic goes here
+    this.filteredFunction.teardown();
+    this.filteredCollection = this.baseCollection.filtered(filterFunction);
+    this.removeSubview(".parks-index", this.parksIndexView);
+    this.parksIndexView = new ThemeParkList.Views.ParksIndex({ 
+      collection: this.filteredCollection 
+    });
+    this.addSubview(".parks-index", this.parksIndexView);
+    this.render();
+  },
 
-    },
+  computeFilter: function () {
 
-    render: function () {
-      this.$el.html(this.template());
-      this.attachSubviews();
-      return this;
-    },
+  },
 
-    swapSubviews: function (newSubview, oldSubview) {
-      this.removeSubview(".parks-index", oldSubview);
-      this.addSubview(".parks-index", newSubview);
-    }
-  })
-);
+  render: function () {
+    this.$el.html(this.template());
+    this.attachSubviews();
+    return this;
+  },
+
+  swapParksIndexView: function (newSubview) {
+    this.removeSubview(".parks-index", this.parksIndexView);
+    this.addSubview(".parks-index", newSubview);
+  }
+});
