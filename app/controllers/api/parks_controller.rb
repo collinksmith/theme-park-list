cd847fdb5b8b6a7aeb4ee822b39d546d24cb1b5c
Collin Smith <collink33@gmail.com>
2015-08-17 16:01:14 -0700

    Add filters for water rides

--- app/controllers/api/parks_controller.rb
+++ app/controllers/api/parks_controller.rb
@@ -44,6 +44,8 @@ class Api::ParksController < ApplicationController
     "hot" => "high >= 85",
     "some roller coasters" => "roller_coasters > 2",
     "many roller coasters" => "roller_coasters > 5",
+    "some water rides" => "water_rides > 2",
+    "many water rides" => "water_rides > 5"
   }
 
   def apply_filters(parks, filters)
