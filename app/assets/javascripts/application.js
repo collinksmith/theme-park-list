8bac7c0a8d4838b22496de663b96ff51b03700a8
Collin Smith <collink33@gmail.com>
2015-08-18 09:47:20 -0700

    Add typeahead

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require underscore
 //= require backbone
 //= require backbone.bootstrap-modal
+//= require typeahead.bundle.js
 //= require theme_park_list
 //= require_tree ../templates
 //= require_tree ./utils
