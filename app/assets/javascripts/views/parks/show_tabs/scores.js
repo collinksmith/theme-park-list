e00e63a44dbc2eb1b148e60501bd9e942ef9a2a9
Collin Smith <collink33@gmail.com>
2015-08-19 11:44:45 -0700

    Update show view * Use grid system, divs, and spans instead of table for layout

--- app/assets/javascripts/views/parks/show_tabs/scores.js
+++ app/assets/javascripts/views/parks/show_tabs/scores.js
@@ -1,7 +1,6 @@
 ThemeParkList.Views.Scores = Backbone.View.extend({
   template: JST["parks/show_tabs/scores"],
   className: "show-details",
-  tagName: "table",
 
   render: function () {
     this.$el.html(this.template({ park: this.model }));
