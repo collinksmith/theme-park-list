0ec2437b2623daf66b66c6b688c9b6ab77f54c5d
Collin Smith <collink33@gmail.com>
2015-08-19 18:29:03 -0700

    Update map stle

--- app/assets/javascripts/views/parks/parks_map.js
+++ app/assets/javascripts/views/parks/parks_map.js
@@ -5,7 +5,7 @@ ThemeParkList.Views.Map = Backbone.View.extend({
 
   initialize: function () {
     this._markers = {};
-    this.styles = [{"stylers":[{"hue":"#ff1a00"},{"invert_lightness":true},{"saturation":-100},{"lightness":33},{"gamma":0.5}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2D333C"}]}]
+    this.styles = [{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#71ABC3"},{"saturation":-10},{"lightness":-21},{"visibility":"simplified"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"hue":"#7DC45C"},{"saturation":37},{"lightness":-41},{"visibility":"simplified"}]},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#C3E0B0"},{"saturation":23},{"lightness":-12},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#A19FA0"},{"saturation":-98},{"lightness":-20},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#FFFFFF"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]}]
   },
 
   initMap: function () {
