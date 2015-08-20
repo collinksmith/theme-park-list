0e3e7845bf18eea898bce7e2cf6f10eec4ddb535
Collin Smith <collink33@gmail.com>
2015-08-20 11:32:06 -0700

    Add handling of submit review

--- app/assets/javascripts/views/parks/park_show.js
+++ app/assets/javascripts/views/parks/park_show.js
@@ -4,6 +4,7 @@ ThemeParkList.Views.ParkShow = Backbone.CompositeView.extend({
   events: {
     "click .show-tab": "updatePanel",
     "click .m-background": "remove",
+    "submit #review-form": "submitReview"
   },
 
   initialize: function () {
@@ -55,5 +56,35 @@ ThemeParkList.Views.ParkShow = Backbone.CompositeView.extend({
   swapInSubview: function (newSubview) {
     this.removeModelSubview(".show-panel", this.model);
     this.addSubview(".show-panel", newSubview);
-  }
+  },
+
+  submitReview: function (event) {
+    event.preventDefault();
+    var formData = $(event.currentTarget).serializeJSON();
+    formData.review.park_id = this.model.id;
+    formData.review.user_id = ThemeParkList.CURRENT_USER.id;
+
+    var newReview = new ThemeParkList.Models.Review();
+    var view = this;
+
+    newReview.save(formData, {
+      success: function () {
+        this.model.fetch({
+          success: function (model) {
+            var newView = ThemeParkList.Views.Reviews({ model: this.model });
+            view.swapInSubview(newView);
+          }
+        })
+      },
+      error: function (model, response) {
+        var errors = _(response.responseJSON);
+        errors.each(function (error) {
+          view.subvews(".show-panel")[0].addError(error);
+        });
+      }
+    });
+    debugger;
+
+
+  },
 });
