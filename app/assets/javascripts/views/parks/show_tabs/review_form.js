fe1627c16fc75989d3c0d86bacfa3d2e3e2c1664
Collin Smith <collink33@gmail.com>
2015-08-20 10:23:23 -0700

    Add raty divs to reviewForm

--- app/assets/javascripts/views/parks/show_tabs/review_form.js
+++ app/assets/javascripts/views/parks/show_tabs/review_form.js
@@ -1,8 +1,35 @@
 ThemeParkList.Views.ReviewForm = Backbone.View.extend({
   template: JST["parks/show_tabs/review_form"],
 
+  initialize: function () {
+     $('.stars').raty({
+      score: 3,
+      starOn: "/icons/star-on.png",
+      starOff: "/icons/star-off.png"
+    });
+
+  },
+
   render: function () {
     this.$el.html(this.template());
+    this.initRaty();
     return this;
+  },
+
+  initRaty: function () {
+    var ratings = ["overall", "atmosphere", "family_friendliness",
+                   "intensity", "wait_times", "cost"]
+    _.each(ratings, this.initRating)
+  },
+
+  initRating: function(name) {
+    var id = "#" + name + "-rating";
+    var scoreName = "review[" + name + "]"
+
+    this.$(id).raty({
+      scoreName: scoreName,
+      starOn: "/icons/star-on.png",
+      starOff: "/icons/star-off.png"
+    })
   }
 });
