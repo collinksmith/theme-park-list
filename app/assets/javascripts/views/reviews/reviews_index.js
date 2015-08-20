0e909f2d5f5b8e350f4e21b6f0e88c8928b46dd6
Collin Smith <collink33@gmail.com>
2015-08-20 16:08:33 -0700

    Add average ratings to reviews view

--- app/assets/javascripts/views/reviews/reviews_index.js
+++ app/assets/javascripts/views/reviews/reviews_index.js
@@ -8,14 +8,48 @@ ThemeParkList.Views.ReviewsIndex = Backbone.CompositeView.extend({
   },
 
   render: function () {
-    this.$el.html(this.template());
+    var reviewsPresent = this.collection.length > 0;
+    this.$el.html(this.template({ reviewsPresent: reviewsPresent }));
     this.attachSubviews();
+
+    if (reviewsPresent) {
+      this.initRaty();
+    }
     return this;
   },
 
   addReviewSubview: function (review) {
-    var reviewSubview = new ThemeParkList.Views.
-                            ReviewsIndexItem({ model : review});
-    this.addSubview(".reviews", reviewSubview);
+      var reviewSubview = new ThemeParkList.Views.
+                              ReviewsIndexItem({ model : review});
+      this.addSubview(".reviews", reviewSubview);
+    },
+
+  initRaty: function () {
+    var ratings = ["overall", "atmosphere", "family_friendliness",
+                   "intensity", "wait_times", "cost"];
+    _.each(ratings, this.initRating.bind(this));
+  },
+
+  initRating: function(name) {
+    var id = "#" + name + "-rating-avg";
+    var allRatings = this.collection.pluck(name);
+    var score = this.average(allRatings);
+
+    this.$(id).raty({
+      readOnly: true,
+      score: score,
+      starOn: "/icons/star-on.png",
+      starOff: "/icons/star-off.png",
+      starHalf: "/icons/star-half.png"
+    });
+  },
+
+  average: function(array) {
+    var total = 0;
+    _.each(array, function(el) {
+      total += el;
+    });
+    var avg = total / array.length;
+    return avg.toFixed(2);
   }
 });
