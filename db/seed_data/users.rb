9e55236dc41aa3482bcb3af18753a99697be8f98
Collin Smith <collink33@gmail.com>
2015-08-21 15:34:30 -0700

    Update seed data - add username to users

--- db/seed_data/users.rb
+++ db/seed_data/users.rb
@@ -1,4 +1,6 @@
 USERS = [{ email: "joe@gmail.com",
+           username: "Joe",
            password: "joejoe" },
          { email: "bob@yahoo.com",
+           username: "Bob",
            password: "bobbob" }]
