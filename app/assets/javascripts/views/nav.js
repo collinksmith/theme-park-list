a1c5939d9adfec49a7bb9d46ed862670a1f2b65f
Collin Smith <collink33@gmail.com>
2015-08-20 17:46:34 -0700

    Add scroll detection to navbar

--- app/assets/javascripts/views/nav.js
+++ app/assets/javascripts/views/nav.js
@@ -1,14 +1,35 @@
 ThemeParkList.Views.Nav = Backbone.View.extend({
   template: JST["nav"],
+  className: "group",
 
   initialize: function (options) {
     this.CURRENT_USER = options.CURRENT_USER;
+    this.previousScroll = 0;
+    $(window).on("scroll", this.handleScroll.bind(this));
   },
 
   render: function () {
     this.$el.html(this.template({ CURRENT_USER: this.CURRENT_USER }));
     this.setTypeahead();
     return this;
+  },
+
+
+  // Adapted from https://stackoverflow.com/questions/17713389/how-to-hide-show-nav-bar-when-user-scrolls-up-down
+  handleScroll: function () {
+    var currentScroll = window.scrollY;
+    var isDown = currentScroll > this.previousScroll;
+
+    if ( isDown && !$("navbar").hasClass('scrolled') ){
+      // scrolling down, didn't add class yet
+      $(".navbar").addClass('scrolled'); // we hide the navbar
+    } else if ( !isDown ){
+      // scrolling up
+      $(".navbar").removeClass('scrolled'); // won't error if no class found
+    }
+
+    // always update position
+    this.previousScroll = currentScroll;
   },
 
   setTypeahead: function () {
