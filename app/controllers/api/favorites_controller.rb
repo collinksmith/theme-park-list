3223bfd5b6e5ee08bd9f364a0022d99ab7824293
Collin Smith <collink33@gmail.com>
2015-08-24 11:25:18 -0700

    Add favorite routes and controller actions

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,2 +1,22 @@
 class Api::FavoritesController < ApplicationController
+  def create
+    @favorite = Favorite.new(favorite_params)
+    if @favorite.save
+      render json: @favorite
+    else
+      render json: @favorite.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy!
+    render json: @favorite
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:park_id, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     resources :parks, only: [:index, :show]
     resources :costs, only: [:index, :show]
     resources :reviews, only: [:index, :show, :create, :update, :destroy]
+    resources :favorites, only: [:create, :destroy]
   end
 end
