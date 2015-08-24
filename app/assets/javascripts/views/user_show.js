e6c261bccdcf85461b5437225652fa9bd29ee73c
Collin Smith <collink33@gmail.com>
2015-08-23 18:56:57 -0700

    Add layout for user show view

--- app/assets/javascripts/views/user_show.js
+++ app/assets/javascripts/views/user_show.js
@@ -1,16 +1,36 @@
 ThemeParkList.Views.UserShow = Backbone.CompositeView.extend({
   template: JST["user_show"],
 
+  events: {
+    "click #my-reviews": "addReviews",
+    "click #my-parks": "addParks"
+  }
+
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
 
     this.navView = new ThemeParkList.Views.Nav({ search: false });
     this.addSubview("#nav", this.navView);
+
+
   },
 
   render: function () {
     this.$el.html(this.template({ user: this.model }));
     this.attachSubviews();
     return this;
-  }
+  },
+
+  addReviews: function () {
+    var reviewsView = new ThemeParkList.Views.ReviewsIndex({
+      collection: this.model.reviews()
+    });
+    this.swapInSubview(reviewsView);
+  },
+
+  swapInSubview: function (newSubview) {
+    this.removeSubview(".show-panel", this.subview);
+    this.subview = newSubview;
+    this.addSubview(".show-panel", this.subview);
+  },
 });
