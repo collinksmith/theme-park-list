eb259dd47753bdaec231c3500b2e99326baf8673
Collin Smith <collink33@gmail.com>
2015-08-23 18:59:27 -0700

    Add parse for backbone user model to parse reviews

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,3 +1,18 @@
 ThemeParkList.Models.User = Backbone.Model.extend({
-  urlRoot: "/api/users"
+  urlRoot: "/api/users",
+
+  parse: function (response) {
+    if (response.reviews) {
+      this.reviews().set(response.reviews)
+      delete response.reviews;
+    }
+    return response;
+  },
+
+  reviews: function () {
+    if (!this._reviews) {
+      this._reviews = new ThemeParkList.Collections.Reviews([], { user: this });
+    }
+    return this._reviews;
+  }
 })
