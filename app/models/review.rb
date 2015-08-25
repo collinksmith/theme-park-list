1faa6a2846f8ae2f7d54c579e39a1ce19e20d95e
Collin Smith <collink33@gmail.com>
2015-08-24 21:55:23 -0700

    Add uniqueness validation on reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ActiveRecord::Base
   validates :user_id, :park_id, :overall, presence: true
   validates :title, length: { maximum: 50 }
+  validates :user_id, uniqueness: { scope: :park_id,
+    message: "cannot review the same park twice" }
 
   belongs_to :park
   belongs_to :user
