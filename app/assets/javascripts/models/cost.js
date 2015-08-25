a45581be2401e25e7a8b151a778264a9c12c2979
Collin Smith <collink33@gmail.com>
2015-08-24 19:56:49 -0700

    Update cost name in park info template

--- app/assets/javascripts/models/cost.js
+++ app/assets/javascripts/models/cost.js
@@ -1,3 +1,12 @@
 ThemeParkList.Models.Cost = Backbone.Model.extend({
-  urlRoot: "/api/costs"
+  urlRoot: "/api/costs",
+
+  toS: function () {
+    var result = "";
+    words = this.escape("cost_type").split("_");
+    _.each(words, function (word) {
+      result += word[0].toUpperCase() + word.slice(1) + " "
+    })
+    return result.trim();
+  }
 });
